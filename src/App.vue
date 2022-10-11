d158b7d8a04264ba828bead80da3b5a815435e08
maniplek <maniplek81@gmail.com>
2022-10-11 13:24:40 +0200

    showing all task

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
  <div class="container">
    <Header title="Task Tracker." />
-  
+  <Tasks :tasks="tasks"/>
  </div>
 
 </template>
 
 <script>
 import Header from './components/Header.vue'
-
+import Tasks  from './components/Tasks.vue'
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
+  },
+   data(){
+        tasks:[]
+    }, created() {
+        this.tasks =[
+            {id:1, text:'Doctors Appointment', day: 'March 1st at 2:30pm', reminder: true,},
+            {id:2, text:'Doctors Appointment', day: 'March 1st at 2:30pm', reminder: true,},
+            {id:3, text:'Doctors Appointment', day: 'March 1st at 2:30pm', reminder: false,},
+        ]
+    },
 
-  }
 }
 </script>
 
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -14,6 +14,6 @@ export default {
             console.log('click...')
         }
     },
-
+   
 }
 </script>
